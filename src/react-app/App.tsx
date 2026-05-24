4cafb5dc167417628274ace9a0fb74c615ac419a
nasimjobinbangalore25-dotcom <nasimjobinbangalore25@gmail.com>
2026-05-24 16:32:31 +0530

    Update App.tsx

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -49,7 +49,7 @@ WELCOME TO VITE REACT WEBSITE BUILD PROCCESS BY CLOULDFLARE
 					count is {count}
 				</button>
 				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
+					Edit <code>src/App.tsx</code> and save to test FOR CLOUD WEBSITE BUILDER OK
 				</p>
 			</div>
 			<div className="card">
